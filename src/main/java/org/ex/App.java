bed899828abb7094eecab1d3e5c23ca59c59a1e1
Zorgit <piligrimexe@Gmail.com>
2024-01-24 00:59:21 +0300

    Add base functionality - read file and sort data by type

--- src/main/java/org/ex/App.java
+++ src/main/java/org/ex/App.java
@@ -6,39 +6,31 @@ import picocli.CommandLine;
         version = "typefilter 1.0",
         description = "get data from text file and extrude to separate file " +
                 "by datatype.")
-public final class App implements Runnable{
+public final class App implements Runnable {
     /**
-     * Some text here.
+     * Picocli parameters
      */
     @CommandLine.Parameters(index = "0",
             description = "Path to the first file")
     private static String filePath1;
 
-    /**
-     * Some text here.
-     */
     @CommandLine.Parameters(index = "1",
             description = "Path to the second file")
     private static String filePath2;
-
-    /**
-     * Some text here.
-     */
     @CommandLine.Option(names = {"-h", "--help"}, usageHelp = true,
             description = "Show this help message and exit.")
     private boolean helpRequested;
 
-    /**
-     * Some text here.
-     */
     @CommandLine.Option(names = {"-V", "--version"}, versionHelp = true,
             description = "Print version information and exit.")
     private boolean versionRequested;
 
+    //test args for run in idea 'run --args="src/test/resources/in1.txt src/test/resources/in2.txt'"
     @Override
     public void run() {
-
+        Filter.generate(filePath1, filePath2);
     }
+
     public static void main(String[] args) {
         CommandLine commandLine = new CommandLine(new App());
         commandLine.addSubcommand("help", new CommandLine.HelpCommand());
--- src/main/java/org/ex/Filter.java
+++ src/main/java/org/ex/Filter.java
@@ -1,5 +1,10 @@
 package org.ex;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+
 public class Filter {
     private Filter() {
     }
@@ -9,5 +14,63 @@ public class Filter {
         return "";
     }
 
+    public static void generate(final String filePath1,
+                                final String filePath2) {
+        readFile(filePath1);
+        readFile(filePath2);
 
+    }
+
+    public static String readFile(final String filePath) {
+        try (BufferedReader reader =
+                     new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                filterLine(line);
+            }
+
+        } catch (IOException e) {
+            //TODO перехватить и обработать ошибки
+            e.printStackTrace();
+        }
+        return "File has been read";
+    }
+
+    private static void filterLine(String line) {
+        String type = isInt(line) ? "int" :
+                (isRealNum(line) ? "real" :
+                        "string");
+        switch (type) {
+            case "real":
+                //TODO запись в структуру данных для статистики и файл
+                System.out.println(line + " is real number");
+                break;
+            case "int":
+                System.out.println(line + " is int number");
+                //TODO запись в структуру данных для статистики и файл
+                break;
+            default:
+                System.out.println(line + " is String");
+                //TODO запись в структуру данных для статистики и файл
+        }
+
+    }
+
+    private static boolean isRealNum(String line) {
+        try {
+            new BigDecimal(line);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static boolean isInt(String line) {
+        try {
+            Integer.parseInt(line);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
