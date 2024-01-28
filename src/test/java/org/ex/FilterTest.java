f534a050e85e0af5a64373815d7512574b7c9cff
Zorgit <piligrimexe@gmail.com>
2024-01-28 22:52:21 +0300

    init commit

---
+++ src/test/java/org/ex/FilterTest.java
@@ -0,0 +1,18 @@
+package org.ex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilterTest {
+
+    @Test
+    void generate() {
+
+    }
+
+    @Test
+    void readAndProcessData() {
+
+    }
+}
---
+++ src/test/java/org/ex/StatsTest.java
@@ -0,0 +1,24 @@
+package org.ex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsTest {
+
+    @Test
+    void showStats() {
+    }
+
+    @Test
+    void getAndShowShort() {
+    }
+
+    @Test
+    void getAndShowFull() {
+    }
+
+    @Test
+    void processRecord() {
+    }
+}
